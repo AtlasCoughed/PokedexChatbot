1623e85f29b6591ca1cf11c5fa647be75857f2c4
Michael Tran <AtlasCoughed@users.noreply.github.com>
2017-02-08 21:07:04 -0800

    [feature] - adding response from recastly

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
  */
 
 const config = {
+    recast: '46c7b63f96c555f29cbb77f130f233d1',
     appId: '3eabc407-450a-4404-87a6-a818bdc5ec3c',
     appPassword: 'XU9iwuPVhBEUs7cULDN076F'
 };
--- pokebot.js
+++ pokebot.js
@@ -4,17 +4,28 @@
 const config = require('./config.js');
 const restify = require('restify');
 const builder = require('botbuilder');
+const recast = require('recastai');
 
 // Connection to Microsoft Bot Framework
 const connector = new builder.ChatConnector({
     appId: config.appId,
     appPassword: config.appPassword
 });
+
+const recastClient = new recast.Client(config.recast)
+
 const bot = new builder.UniversalBot(connector);
 // Event when Message received
 bot.dialog('/', (session) => {
-    console.log(session.message.text)
-})
+    recastClient.textRequest(session.message.text)
+        .then(res => {
+            const intent = res.intent();
+            const entity = res.get('pokemon');
+            session.send(`Intent: ${intent.slug}`)
+            session.send(`Entity: ${entity.name}`)
+        })
+    .catch(() => session.send('I need some sleep right now... Talk to me later!'))
+});
 
 // Server Init
 const server = restify.createServer();
