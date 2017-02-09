377f296d9d2fe1ba9a5debd6997c9214e028583f
Michael Tran <AtlasCoughed@users.noreply.github.com>
2017-02-08 21:13:06 -0800

    [feature] - PokeAPI

--- pokebot.js
+++ pokebot.js
@@ -1,33 +1,34 @@
 /**
  * Created by miketran on 2/8/17.
  */
+const getGreetings = require('./intents/greetings.js');
+const getInfoPokemon = require('./intents/infopokemon.js');
 const config = require('./config.js');
 const restify = require('restify');
 const builder = require('botbuilder');
-const recast = require('recastai');
-
 // Connection to Microsoft Bot Framework
 const connector = new builder.ChatConnector({
     appId: config.appId,
-    appPassword: config.appPassword
-});
-
-const recastClient = new recast.Client(config.recast)
-
+    appPassword: config.appPassword,
+})
 const bot = new builder.UniversalBot(connector);
-// Event when Message received
+const INTENTS = {
+    greetings: getGreetings,
+    infopokemon: getInfoPokemon,
+}
+
 bot.dialog('/', (session) => {
     recastClient.textRequest(session.message.text)
-        .then(res => {
-            const intent = res.intent();
-            const entity = res.get('pokemon');
-            session.send(`Intent: ${intent.slug}`)
-            session.send(`Entity: ${entity.name}`)
-        })
-    .catch(() => session.send('I need some sleep right now... Talk to me later!'))
-});
-
+    .then(res => {
+    const intent = res.intent()
+    const entity = res.get('pokemon')
+    if (intent) {
+        session.send(INTENTS[intent.slug](entity))
+    }
+})
+.catch(() => session.send('I need some sleep right now... Talk to me later!'))
+})
 // Server Init
-const server = restify.createServer();
+const server = restify.createServer()
 server.listen(8080)
-server.post('/', connector.listen());
+server.post('/', connector.listen())
