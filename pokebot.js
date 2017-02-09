f700855ee468ac4adf9b320b74137ad3675bf82c
Michael Tran <AtlasCoughed@users.noreply.github.com>
2017-02-08 20:50:50 -0800

    [feature] - hooked up facebook messenger to botframework

--- pokebot.js
+++ pokebot.js
@@ -1,7 +1,22 @@
 /**
  * Created by miketran on 2/8/17.
  */
+const config = require('./config.js');
+const restify = require('restify');
+const builder = require('botbuilder');
 
-const restify = require('restify')
-const builder = require('botbuilder')
-const server = restify.createServer().listen(8080)
+// Connection to Microsoft Bot Framework
+const connector = new builder.ChatConnector({
+    appId: config.appId,
+    appPassword: config.appPassword
+});
+const bot = new builder.UniversalBot(connector);
+// Event when Message received
+bot.dialog('/', (session) => {
+    console.log(session.message.text)
+})
+
+// Server Init
+const server = restify.createServer();
+server.listen(8080)
+server.post('/', connector.listen());
